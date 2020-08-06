12dce440ccdb5959843d8a84ba57b9f565d399b7
Apranzo <al.podobed@gmail.com>
2020-08-06 18:09:02 +0300

    blaydва333100500

--- src/lem_in.c
+++ src/lem_in.c
@@ -256,43 +256,50 @@ int			parse_ants_amount(t_lemin *lem)
 static	void 		check_unuses(t_room	*room)
 {
 	t_node			*node;
+	t_node			*next;
 	pointer			*data;
 
 //	room = pair->value;
 	node = room->out->first;
 	while (node)
 	{
+		next = node->next;
+		data = node->data;
 		if (room->asc_level < 0)
 		{
-			lst_rm_data(((t_room *) node->data)->out, (f_equal) &room_equals, room);
-			lst_rm_data(((t_room *) node->data)->in, (f_equal) &room_equals, room);
-			lst_rm_data(room->in, (f_equal) &room_equals, node->data);
-			lst_rm_data(room->out, (f_equal) &room_equals, node->data);
+			lst_rm_data(((t_room *) data)->out, (f_equal) &room_equals, room);
+			lst_rm_data(((t_room *) data)->in, (f_equal) &room_equals, room);
+			lst_rm_data(room->in, (f_equal) &room_equals, data);
+			lst_rm_data(room->out, (f_equal) &room_equals, data);
 		}
 		else if (((t_room*)node->data)->asc_level == room->asc_level)
 		{
-			lst_rm_data(room->in, (f_equal) &room_equals, node->data);
-			lst_rm_data(((t_room *) node->data)->out, (f_equal) &room_equals, room);
-			lst_rm_data(room->out, (f_equal) &room_equals, node->data);
-			lst_rm_data(((t_room *) node->data)->in, (f_equal) &room_equals, room);
+			lst_rm_data(room->in, (f_equal) &room_equals, data);
+			lst_rm_data(((t_room *) data)->out, (f_equal) &room_equals, room);
+			lst_rm_data(room->out, (f_equal) &room_equals, data);
+			lst_rm_data(((t_room *) data)->in, (f_equal) &room_equals, room);
 		}
-		node = node->next;
+		node = next;
 	}
 }
 
 static	void 		alight(t_room	*room)
 {
 	t_node			*node;
-//	t_node			*tmp; TODO free maybe
+	t_node			*next;
+	pointer 		data;
+
 	node = room->out->first;
 	while (node)
 	{
-		if (((t_room*)node->data)->asc_level < room->asc_level)
+		data = node->data;
+		next = node->next;
+		if (((t_room*)data)->asc_level < room->asc_level)
 		{
-			lst_rm_data(room->out, (f_equal) &room_equals, node->data);
-			lst_rm_data(((t_room *) node->data)->in, (f_equal) &room_equals, room);
+			lst_rm_data(room->out, (f_equal) &room_equals, data);
+			lst_rm_data(((t_room *) data)->in, (f_equal) &room_equals, room);
 		}
-		node = node->next;
+		node = next;
 	}
 }
 
